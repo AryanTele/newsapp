a6d8952fe71d187170db0724724e54db0da82f4d
Aryan Tele <telearyan12@gmail.com>
2024-06-14 16:14:26 +0530

    fetch image, title, and desc from news api

--- news app/index.js
+++ news app/index.js
@@ -5,7 +5,7 @@ const blogContainer = document.getElementById("blog-container");
 async function fetchRandomNews() {
   try {
     //fetch data
-    const apiURL = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${API_KEY}`;
+    const apiURL = `https://newsapi.org/v2/top-headlines?country=us&pageSize=8&apikey=${API_KEY}`;
     const response = await fetch(apiURL);
     const data = await response.json();
     console.log(data);
@@ -16,13 +16,43 @@ async function fetchRandomNews() {
     return [];
   }
 }
-function displayBlock(articles) {}
+function displayBlock(articles) {
+  blogContainer.innerHTML = "";
+  articles.forEach((article) => {
+    const blogCard = document.createElement("div");
+    blogCard.classList.add("blog-card");
 
-async () => {
+    const img = document.createElement("img");
+    img.src = article.urlToImage;
+    img.alt = article.title;
+    const title = document.createElement("h2");
+    const trucatedTitle =
+      article.title.length > 30
+        ? article.title.slice(0, 30) + "..."
+        : article.title;
+    const description = document.createElement("p");
+
+    const trucatedDesc =
+      article.description.length > 120
+        ? article.description.slice(0, 120) + "..."
+        : article.description;
+    description.textContent = trucatedDesc;
+    title.textContent = trucatedTitle;
+    const blogText = document.createElement("div");
+    blogText.classList.add("blog-text");
+    blogCard.appendChild(img);
+    blogCard.appendChild(title);
+    blogCard.appendChild(description);
+
+    blogContainer.appendChild(blogCard);
+  });
+}
+
+(async () => {
   try {
     const articles = await fetchRandomNews();
     displayBlock(articles);
   } catch (error) {
     console.log("Error fetching data", error);
   }
-};
+})();
